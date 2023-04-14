9fd1d3c1707a0401d143eee6c92c2b4114647dda
Zhen Wang <97012411+z246wang@users.noreply.github.com>
2023-04-13 19:20:56 -0500

    Add files via upload

--- src/main/java/Test/t_TestStrategy.java
+++ src/main/java/Test/t_TestStrategy.java
@@ -4,10 +4,9 @@ import java.awt.Font;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -22,7 +21,16 @@ import org.apache.commons.math3.stat.inference.TTest;
  */
 
 public class t_TestStrategy implements TestStrategy {
-	
+			
+	JFrame frame = new JFrame();
+	JLabel degreesOfFreedomResult;
+	JLabel tResult;
+	JLabel pResult;
+	JLabel statement;
+	JLabel DOF = new JLabel("Degrees of Freedom:");
+	JLabel t_value = new JLabel("t-value:");
+	JLabel p_value = new JLabel("p-value:");
+		
 	/**
 	 * Performs a t-test based on the given result set, and the 2 queries given.
 	 * @param result the resultSet needed for the t-test
@@ -32,65 +40,14 @@ public class t_TestStrategy implements TestStrategy {
 	
 	public void t_Test(ResultSet result, String query1, String query2) {
 		
-		JFrame frame = new JFrame();
-		JLabel degreesOfFreedomResult;
-		JLabel tResult;
-		JLabel pResult;
-		JLabel statement;
-		
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/echodata", "root", "password");
-			
-			ResultSet rs1;
-			ResultSet rs2;
-			
-			ArrayList<Double> resultSample1 = new ArrayList<Double>();
-			Statement statement1 = connection.prepareStatement(query1);
-			rs1 = statement1.executeQuery(query1);
-			
-			while (rs1.next()) {
-				String input1Str = rs1.getString(1);
-				Double input1 = null;
-				if (input1Str != null) {
-					input1 = Double.parseDouble(rs1.getString(1));
-				}
-				resultSample1.add(input1);
-			}
-			
-			List<Double> resultSample2 = new ArrayList<Double>();
-			Statement statement2 = connection.prepareStatement(query2);
-			rs2 = statement2.executeQuery(query2);
-			
-			while (rs2.next()) {
-				String input2Str = rs2.getString(1);
-				Double input2 = null;
-				if (input2Str != null) {
-					input2 = Double.parseDouble(rs2.getString(1));
-				}
-				resultSample2.add(input2);
-			}
-			
-			double[] result1 = new double[resultSample1.size()];
-			for (int i = 0; i < resultSample1.size(); i++) {
-				if (resultSample1.get(i) != null) {
-					result1[i] = resultSample1.get(i);
-				}
-			}
-			DescriptiveStatistics stats1 = new DescriptiveStatistics(result1);
-			
-			double[] result2 = new double[resultSample2.size()];
-			for (int i = 0; i < resultSample2.size(); i++) {
-				if (resultSample2.get(i) != null) {
-					result2[i] = resultSample2.get(i);
-				}
-			}
-			DescriptiveStatistics stats2 = new DescriptiveStatistics(result2);
-			
+			DescriptiveStatistics stats1 = new DescriptiveStatistics(retrieveData(query1));
+			DescriptiveStatistics stats2 = new DescriptiveStatistics(retrieveData(query2));
 			//t_Test formula performing
 			double degreesOfFreedom = stats1.getN() + stats2.getN() - 2;
 			if (degreesOfFreedom <= 0) {
-				JOptionPane.showMessageDialog(null, "Not enough data, please choose a longer time interval or choose a different location.", "Error", JOptionPane.ERROR_MESSAGE);
-
+				JOptionPane.showMessageDialog(null, "Not enough data, please choose a longer time interval or choose a different location.",
+											"Error", JOptionPane.ERROR_MESSAGE);
 			} else {
 				TTest tTest = new TTest();
 				double t = tTest.t(stats1, stats2);
@@ -102,48 +59,80 @@ public class t_TestStrategy implements TestStrategy {
 				}else {
 					statement = new JLabel("We can not reject the null hypothesis.");
 				}
-				
-				//Create label for result
-				degreesOfFreedomResult = new JLabel(Double.toString(degreesOfFreedom));
-				tResult = new JLabel(Double.toString(t));
-				pResult = new JLabel(Double.toString(p));
-				
-				JLabel DOF = new JLabel("Degrees of Freedom:");
-				JLabel t_value = new JLabel("t-value:");
-				JLabel p_value = new JLabel("p-value:");
-				
-				frame.setTitle("T_Test Results");
-			    frame.setLayout(null);
-			    frame.setSize(400,400);
-			    frame.setVisible(true);
-			    
-			    frame.add(DOF);
-			    frame.add(degreesOfFreedomResult);
-			    frame.add(t_value);
-			    frame.add(tResult);
-			    frame.add(p_value);
-			    frame.add(pResult);
-			    frame.add(statement);
-	 
-			    DOF.setBounds(50, 80, 150, 50);
-			    degreesOfFreedomResult.setBounds(200, 80, 150, 50);
-			    t_value.setBounds(50, 120, 150, 50);
-			    tResult.setBounds(200, 120, 150, 50);
-			    p_value.setBounds(50, 160, 150, 50);
-			    pResult.setBounds(200, 160, 150, 50);
-			    statement.setBounds(50, 200, 300, 50);
-			    DOF.setFont(new Font("", Font.PLAIN, 15));
-			    t_value.setFont(new Font("", Font.PLAIN, 15));
-			    degreesOfFreedomResult.setFont(new Font("", Font.PLAIN, 15));
-			    tResult.setFont(new Font("", Font.PLAIN, 15));
-			    p_value.setFont(new Font("", Font.PLAIN, 15));
-			    pResult.setFont(new Font("", Font.PLAIN, 15));
-			    statement.setFont(new Font("", Font.PLAIN, 15));
-			}
-		}
-		catch (Exception e) {
+				setupFrame(degreesOfFreedom, t, p);
+			} 
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		    
+	}
+	
+	/**
+	 * retrieves the data from selected location.
+	 * @param query the query needed to retrieve the data
+	 */
+	
+	private double[] retrieveData(String query) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/echodata", "root", "password");
+		ResultSet rs;
+		ArrayList<Double> selectedData = new ArrayList<Double>();
+		Statement statement = connection.prepareStatement(query);
+		rs = statement.executeQuery(query);
+		while (rs.next()) {
+			String inputStr = rs.getString(1);
+			Double input = null;
+			if (inputStr != null) {
+				input = Double.parseDouble(rs.getString(1));
+			}
+			selectedData.add(input);
+		}
+		double[] result = new double[selectedData.size()];
+		for (int i = 0; i < selectedData.size(); i++) {
+			if (selectedData.get(i) != null) {
+				result[i] = selectedData.get(i);
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * set up the frame to show the t-test result
+	 * @param degreesOfFreedom the degrees of freedom from the selected data
+	 * @param t the t value from the t-test 
+	 * @param p the p value from the t-test
+	 */
+	
+	private void setupFrame(double degreesOfFreedom, double t, double p) {
+		
+		degreesOfFreedomResult = new JLabel(Double.toString(degreesOfFreedom));
+		tResult = new JLabel(Double.toString(t));
+		pResult = new JLabel(Double.toString(p));
+		
+		frame.setTitle("T_Test Results");
+	    frame.setLayout(null);
+	    frame.setSize(400,400);
+	    frame.setVisible(true);
+	    
+	    frame.add(DOF);
+	    frame.add(degreesOfFreedomResult);
+	    frame.add(t_value);
+	    frame.add(tResult);
+	    frame.add(p_value);
+	    frame.add(pResult);
+	    frame.add(statement);
+
+	    DOF.setBounds(50, 80, 150, 50);
+	    degreesOfFreedomResult.setBounds(200, 80, 150, 50);
+	    t_value.setBounds(50, 120, 150, 50);
+	    tResult.setBounds(200, 120, 150, 50);
+	    p_value.setBounds(50, 160, 150, 50);
+	    pResult.setBounds(200, 160, 150, 50);
+	    statement.setBounds(50, 200, 300, 50);
+	    DOF.setFont(new Font("", Font.PLAIN, 15));
+	    t_value.setFont(new Font("", Font.PLAIN, 15));
+	    degreesOfFreedomResult.setFont(new Font("", Font.PLAIN, 15));
+	    tResult.setFont(new Font("", Font.PLAIN, 15));
+	    p_value.setFont(new Font("", Font.PLAIN, 15));
+	    pResult.setFont(new Font("", Font.PLAIN, 15));
+	    statement.setFont(new Font("", Font.PLAIN, 15));
 	}
 }
